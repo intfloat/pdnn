7f7ba73530f21012be6b918e36c336b8c4c015fc
wangliang <wangliangpeking@gmail.com>
2015-12-12 09:36:35 +0800

    Fix boundary issue

--- learning/sgd.py
+++ learning/sgd.py
@@ -63,8 +63,8 @@ def train_sgd_verbose(train_fn, train_sets, train_xy, batch_size, learning_rate,
 def train_sgd(train_fn, cfg):
     train_sets = cfg.train_sets; train_xy = cfg.train_xy
     batch_size = cfg.batch_size
-    learning_rate = cfg.lrate.get_rate(); momentum = cfg.momentum 
-    
+    learning_rate = cfg.lrate.get_rate(); momentum = cfg.momentum
+
     train_error = []
     while (not train_sets.is_finish()):
         train_sets.load_next_partition(train_xy)
